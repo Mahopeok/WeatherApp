51712d70971fcd0c353240dd5873325fda8b6d47
Mahopeok <hopemaha11@gmail.com>
2025-04-30 04:11:55 +0100

    included the Api and key. manipulated the innerhtml for the city and temperature value

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+function searchCity(city) {
+    let apiKey = "bd13391t3d87a406o7b70e7f7fa17f8e";            
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayweather);
+}
 function displayweather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = Math.round(response.data.main.temp);
-    temperatureElement.innerHTML = `${temperature}°C`;
+    let temperatureElement = document.querySelector("#temperature-value");
+    let temperature = Math.round(response.data.temperature.current);
+    let city = document.querySelector("#display-city");
+    city.innerHTML = response.data.city;
+    temperatureElement.innerHTML = `${temperature}`;
 }
 
 
@@ -9,8 +16,10 @@ function displayweather(response) {
 function search(event){
     event.preventDefault(); 
     let searchInput = document.querySelector("#search-input");
-    let city = document.querySelector("#display-city");
-    city.innerHTML = searchInput.value;
+    
+    
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",search);
+searchFormElement.addEventListener("submit", search);
+searchCity("Abuja");
