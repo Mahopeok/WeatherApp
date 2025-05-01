02b66a3254d5f47586534013e63a48dfacfc9160
Mahopeok <hopemaha11@gmail.com>
2025-05-01 12:23:17 +0100

    generating live values for humidity, time and windspeed in js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ function displayweather(response) {
     let city = document.querySelector("#display-city");
     city.innerHTML = response.data.city;
     temperatureElement.innerHTML = `${temperature}`;
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.condition.description;
+    let humidityElement = document.querySelector("#Humidity");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    let windSpeedElement = document.querySelector("#wind-speed");
+    windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+    console.log(response.data);
 }
 
 
@@ -23,3 +30,14 @@ function search(event){
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
 searchCity("Abuja");
+
+let now = new Date();
+let minute = now.getMinutes();
+let hour = now.getHours()
+let date = now.getDate()
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let day = days[now.getDay()]
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let month = months[now.getMonth()];
+let dateTimeElement = document.querySelector("#date-time");
+dateTimeElement.innerHTML = `${day}, ${date} ${month} ${hour}:${minute}`;
